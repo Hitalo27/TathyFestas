3405da4515b1367e587c7a19c78a250adfa1ce1b
Hitalo Silveira <hitaloporto1@gmail.com>
2024-05-09 17:48:58 -0300

    Alterações

--- Codigo/browsestore/src/main/java/com/tathyfestas/TathyFestaApplication.java
+++ Codigo/browsestore/src/main/java/com/tathyfestas/TathyFestaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrowsestoreApplication {
+public class TathyFestaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrowsestoreApplication.class, args);
+		SpringApplication.run(TathyFestaApplication.class, args);
 	}
 
 }
